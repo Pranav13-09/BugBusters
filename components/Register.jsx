b424accdcc6750c079f3dccdf8779b8ed780c752
Faiz-Sayyed <spfaizali02@gmail.com>
2023-12-19 11:14:21 +0530

    Login Register Toggle

--- components/Register.jsx
+++ components/Register.jsx
@@ -1,27 +1,29 @@
 import React, { useState } from "react";
 import axios from "axios";
-import { useRouter } from "next/router";
 
-const Register = (props) => {
-  const router = useRouter();
+const Register = ({ setCurrentForm }) => {
   const [email, setEmail] = useState("");
   const [pass, setPass] = useState("");
   const [name, setName] = useState("");
   const [type, setType] = useState("");
+
+  const showLogin = () => {
+    setCurrentForm("login");
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
       console.log("I am sending");
       console.log(name, email, pass, type, "i am all the info");
-        const response = await axios.post(`/api/auth/register`, {
-          name: name,
-          email: email,
-          password: pass,
-          type: type,
-        });
-        console.log("I am sent");
-        console.log(response, "i am response");
+      const response = await axios.post(`/api/auth/register`, {
+        name: name,
+        email: email,
+        password: pass,
+        type: type,
+      });
+      console.log("I am sent");
+      console.log(response, "i am response");
     } catch (e) {
       console.log(e, "i am error");
     }
@@ -29,10 +31,10 @@ const Register = (props) => {
 
   return (
     <div className="flex w-screen h-screen justify-center items-center bg-slate-300">
-      <img src="/logo.jpg" className="w-7/12 h-2/3" />
-      <div className="flex flex-col items-center justify-center w-4/12 h-2/3 p-5 bg-slate-200">
+      <img src="/logo.jpg" className="w-7/12 h-3/4" />
+      <div className="flex flex-col items-center justify-center w-4/12 h-3/4 p-5 bg-slate-200">
         <h1 className="text-2xl text-blue-900 font-bold">Register</h1>
-        <div className="w-4/5 text-lg mt-2">Name</div>
+        <div className="w-4/5 text-md mt-2">Name</div>
         <input
           type="text"
           placeholder="Name"
@@ -71,6 +73,12 @@ const Register = (props) => {
           className="w-4/5 text-xl mt-5 p-2 text-center rounded-lg text-white bg-blue-900 hover:cursor-pointer "
         >
           Register
+        </div>
+        <div
+          onClick={showLogin}
+          className="flex w-4/5 text-blue-500 cursor-pointer justify-center mt-1"
+        >
+          Already have an account? Login
         </div>
       </div>
     </div>
--- pages/index.js
+++ pages/index.js
@@ -10,20 +10,12 @@ const App = () => {
 
   return (
     <>
-      <Login />   
-      <Register/>
+      {currentForm === "login" ? (
+        <Login setCurrentForm={setCurrentForm} />
+      ) : (
+        <Register setCurrentForm={setCurrentForm} />
+      )}
     </>
- 
-    // <div className="container">
-    //   <div className="left_container">
-    //     <img src="/bg5.png" alt="About Image" />
-    //   </div>
-    //   <div className="right_container">
-    //     <div className="right_inside">
-    //       {currentForm === "login" ? <Login onFormSwitch={toggleForm} /> : <Register onFormSwitch={toggleForm} />}
-    //     </div>
-    //   </div>
-    // </div>
   );
 };
 
